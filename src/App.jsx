fdad0ae5c16031030f94a03b2c32eeab92eb941e
Bouakkaz Lotfi <61977293+BouakkazLotfiG@users.noreply.github.com>
2022-08-14 18:56:22 +0100

    intro nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './style'
 
+import {Navbar, Billing, Business, Stats, CardDeal, Testimonials, Clients, CTA, Footer, Hero } from './components'
+
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
@@ -9,28 +11,28 @@ const App = () => {
       {/* navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          navbar
+          <Navbar />
         </div>
       </div>
 
       {/* hero section  */}
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          hero
+          <Hero />
         </div>
       </div>
 
-      {/* hero section  */}
+      {/* rest section  */}
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          stat
-          buisness
-          bill
-          card default
-          test
-          clie,t
-          cta
-          footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
