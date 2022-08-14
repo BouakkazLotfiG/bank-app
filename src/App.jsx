28b95873b038511a439ad20ca83814d03f554ff5
Bouakkaz Lotfi <61977293+BouakkazLotfiG@users.noreply.github.com>
2022-08-14 18:32:57 +0100

    nice improv

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import styles from './style'
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <h1>heloo nigga</h1>
+
+
+      {/* navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          navbar
+        </div>
+      </div>
+
+      {/* hero section  */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          hero
+        </div>
+      </div>
+
+      {/* hero section  */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          stat
+          buisness
+          bill
+          card default
+          test
+          clie,t
+          cta
+          footer
+        </div>
+      </div>
+
+
+
     </div>
   )
 }
